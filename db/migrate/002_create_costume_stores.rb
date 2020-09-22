5570d737f86544ab28b74b4d6c6eb8300d1f7b02
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-22 01:41:48 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,16 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :costume_stores do |y|
+      y.string :name 
+      y.string :location
+      y.integer :costume_inventory
+      y.integer :num_of_employees
+      y.boolean :still_in_business
+      y.datetime :start_time
+      y.datetime :end_time
+    end 
+  end 
+end 
+      
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+    def change
+        create_table :haunted_houses do |z|
+            z.string :name
+            z.string :location
+            z.string :theme
+            z.float :price
+            z.boolean :family_friendly
+            z.datetime :opening_date
+            z.datetime  :closing_date
+            z.text :description
+        end
+    end
+end

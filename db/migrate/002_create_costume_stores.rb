bb21a208ce2f477c6ae2ed9fdba52620889e29da
Kenneth Colon <kennethmanuel250@gmail.com>
2020-09-22 02:12:13 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,5 +1,5 @@
 # Create your costume_stores migration here
-class CostumeStore < ActiveRecord::Migration[5.2]
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
   
   def change 
     create_table :costume_stores do |y|
@@ -8,8 +8,8 @@ class CostumeStore < ActiveRecord::Migration[5.2]
       y.integer :costume_inventory
       y.integer :num_of_employees
       y.boolean :still_in_business
-      y.datetime :start_time
-      y.datetime :end_time
+      y.datetime :opening_time
+      y.datetime :closing_time
     end 
   end 
 end 
